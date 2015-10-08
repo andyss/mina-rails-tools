0758d03d9c0e49ca4d7aa16bea707a2f343d2633
Joey Lin <joeyoooooo@gmail.com>
2015-10-09 00:18:49 +0800

    init gem

---
+++ lib/mina/extras.rb
@@ -0,0 +1,7 @@
+require "mina/extras/version"
+
+module Mina
+  module Extras
+    # Your code goes here...
+  end
+end
---
+++ lib/mina/extras/version.rb
@@ -0,0 +1,5 @@
+module Mina
+  module Extras
+    VERSION = "0.1.0"
+  end
+end
