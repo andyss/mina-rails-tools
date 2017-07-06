38cb166a6a235fc11287b9ca7d51a98c390f1ffa
Joey Lin <joeyoooooo@gmail.com>
2017-07-06 17:11:25 +0800

    migrate to new mina

--- lib/mina/extras/nginx.rb
+++ lib/mina/extras/nginx.rb
@@ -5,34 +5,34 @@ set :nginx_port, 80
 set :nginx_default, false
 
 namespace :nginx do
-    
+
   # desc "Nginx: Setup config files"
   # task :setup => [:upload, :link]
-  
+
   desc "Nginx: Parses config file and uploads it to server"
   task :upload do
     upload_shared_file("nginx.conf")
   end
-  
+
   desc "Nginx: Symlink config file"
   task :link do
     invoke :sudo
     extra_echo("Nginx: Symlink config file")
-    
-    command %{sudo ln -fs "#{fetch(:deploy_to)}/shared/nginx.conf" "#{nginx_conf_path}/#{fetch(:app)}.conf"}
+
+    command %{sudo ln -fs "#{fetch(:deploy_to)}/shared/nginx.conf" "#{fetch(:nginx_conf_path)}/#{fetch(:app)}.conf"}
     # command check_symlink nginx_conf_path
   end
-    
+
   desc "Current nginx config"
   task :config do
     invoke :sudo
-    
+
     command_echo("#"*80)
-    command_echo("# File: #{nginx_conf_path}/#{fetch(:app)}.conf")
-    command %{sudo cat "#{nginx_conf_path}/#{fetch(:app)}.conf"}
+    command_echo("# File: #{fetch(:nginx_conf_path)}/#{fetch(:app)}.conf")
+    command %{sudo cat "#{fetch(:nginx_conf_path)}/#{fetch(:app)}.conf"}
     command_echo("#"*80)
   end
-  
+
   %w(stop start restart reload status).each do |action|
     desc "#{action.capitalize} Nginx"
     task action.to_sym do
